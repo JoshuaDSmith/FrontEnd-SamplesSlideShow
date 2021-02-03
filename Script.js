43364dd2f083eaac12d862237a669a662bee7f51
Joshua <calebdouglas.j@gmail.com>
2021-02-03 12:13:32 +0000

    first commit

---
+++ Script.js
@@ -0,0 +1,35 @@
+console.log("hello");
+
+var slides = document.querySelectorAll("#slides .slide");
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 2000);
+
+function nextSlide() {
+  slides[currentSlide].className = "slide";
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = "slide showing";
+}
+
+// Second Manual toggle Slideshow
+
+var quotes = document.querySelectorAll("#Quotes .quoteitems");
+var currentQuote = 0;
+
+function nextQuote() {
+  console.log(currentQuote);
+  if (currentQuote <= 3) {
+    quotes[currentQuote].className = "quoteitems";
+    currentQuote = currentQuote + 1;
+    quotes[currentQuote].className = "quote showing";
+  }
+}
+
+function prevQuote() {
+  console.log(currentQuote);
+
+  if (currentQuote >= 1) {
+    quotes[currentQuote].className = "quoteitems";
+    currentQuote = (currentQuote - 1) % quotes.length;
+    quotes[currentQuote].className = "quote showing";
+  }
+}
